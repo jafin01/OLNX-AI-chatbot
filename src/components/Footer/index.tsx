a888d9de201cef3eea6bdf005b5f3d4dd0b9d8b4
kusumachandu <kusumachandu43@gmail.com>
2023-04-03 17:01:05 +0530

    ✨landing page mobile responsiveness added

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-zinc-900 text-gray-400 py-20">
+      <div className="px-5">
+        <p>
+          Sample text. Click to select the text box. Click again or double click
+          to start editing the text.
+        </p>
+      </div>
+      <div className="px-5 pt-10">
+        <p>Website Templates created with Website Builder Software.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,102 +1,211 @@
-import Link from "next/link";
-import { useEffect, useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import Footer from "@/components/Footer";
+import React from "react";
 
-export default function Home() {
-  let [accessToken, setAccessToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (window.localStorage.getItem("accessToken")) {
-      setAccessToken(window.localStorage.getItem("accessToken"));
-    }
-  }, []);
-
+function index() {
   return (
-    <main className="h-screen flex w-full">
-      <section className="bg-cred h-full w-full flex flex-col">
-        <div className="flex-1 flex flex-col">
-          <nav className="font-mono font-extrabold text-lg p-6">OLNX</nav>
-          <section className="relative flex-1">
-            <img
-              className="w-96 absolute bottom-0 left-12"
-              src="/images/bot-1-transformed.png"
-              alt="Photo of two AI bots talking to one another."
-            />
-          </section>
+    <div>
+      <div className="bg-cred-light">
+        <div className="py-5 flex justify-end">
+          <h1>Feather icons</h1>
         </div>
-        <div className="h-96 bg-cred-light p-12 flex flex-col gap-2">
-          <h2 className="text-3xl font-bold uppercase">Welcome To OLNX</h2>
-          <p className="font-mono">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
-            placeat perferendis qui consequuntur nesciunt architecto! Odio
-            incidunt porro aut doloremque, dolore id eveniet quidem dignissimos
-            dolorem officiis commodi, recusandae omnis?
+        <div className="text-white font-semibold text-3xl w-[60%] m-auto pb-10">
+          <h1>
+            Sales Process Deisgn & Methodology
+          </h1>
+        </div>
+        <div className="w-[60%] md:w-[70%] m-auto pb-10">
+          <img
+            className="w-[100%] h-[100%]"
+            src="https://images01.nicepage.com/a1389d7bc73adea1e1c1fb7e/376109ea795c5348931d0f77/pexels-photo-2425025.jpeg"
+            alt="image"
+          />
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="border bg-gray-200 text-center w-[65%] m-auto py-10 px-1">
+          <h1 className="text-3xl font-semibold">
+            we collaborte with brands and agencies to create memorable
+            experiences.
+          </h1>
+        </div>
+        <div className="bg-cgreen-light w-[120px] p-5 mt-6 ml-[18%]">
+          <h1 className="text-center font-black text-6xl text-white">01</h1>
+        </div>
+        <div className="w-[65%] m-auto pt-5 pb-14">
+          <p>
+            Sample text. Click to select the text box. Click again or double
+            click to start editing the text.
           </p>
-          <div className="flex">
-            <span className="flex-1" />
-            <Link
-              href="/login"
-              className="px-6 py-3 bg-cred-dark rounded-full border-4 border-cred"
-            >
-              Get Started
-            </Link>
+        </div>
+        <div className="w-[65%] m-auto">
+          <p className="font-bold">
+            The principal reason we continue to adapt and evolve our business
+            model is to ensure that we are meeting our customers expectations.
+            One example of this has been to use modern technology and the
+            introduction of the real time tracking our teams using GPS.
+          </p>
+        </div>
+        <div className="w-[65%] m-auto pt-14">
+          <p>
+            Sample text. Click to select the text box. Click again or double
+            click to start editing the text.
+          </p>
+        </div>
+        <div className="bg-cgreen-light w-[120px] p-5 mt-6 ml-[18%]">
+          <h1 className="text-center font-black text-6xl text-white">02</h1>
+        </div>
+      </div>
+      <div className="bg-cred-light mt-14">
+        <div className="w-[65%] m-auto py-14">
+          <div>
+            <h1 className="font-bold text-3xl pb-5">one.</h1>
+            <p>
+              Nec feugiat nisl pretium fusce id. Justo laoreet sit amet cursus
+              sit amet. Porta non pulvinar neque laoreet suspendisse interdum
+              consectetur libero.
+            </p>
+          </div>
+          <div className="pt-10">
+            <h1 className="font-bold text-3xl pb-5">two.</h1>
+            <p>
+              Amet luctus venenatis lectus magna fringilla urna porttitor
+              rhoncus dolor. A lacus vestibulum sed arcu non. Dolor magna eget
+              est lorem ipsum dolor sit amet consectetur. Mauris pellentesque
+              pulvinar pellentesque habitant morbi tristique senectus.
+            </p>
+          </div>
+          <div className="pt-14">
+            <img src="../images/pexels-photo.jpeg" alt="feather" />
           </div>
         </div>
-      </section>
-      <section className="bg-cgreen-dark h-full w-full flex flex-col">
-        <div className="flex-1 flex flex-col">
-          <nav className="flex flex-row p-6 gap-4 items-center">
-            <span className="flex-1" />
-
-            {accessToken ? (
-              <>
-                <Link href="/playgrounds">Playgrounds</Link>
-                {/* <Link href="/billing">Billing</Link> */}
-                <Link
-                  href="/logout"
-                  className="p-2 bg-cgreen-light rounded-full flex gap-2 items-center"
-                >
-                  <img
-                    className="w-10 h-10 bg-cgreen-dark rounded-full"
-                    src="https://api.dicebear.com/5.x/micah/svg"
-                    alt="Profile Picture"
-                  />
-                  <span>Logout</span>
-                </Link>
-              </>
-            ) : (
-              <>
-                <Link href="/login">Login</Link>
-                {/* <Link href="/register">Register</Link> */}
-              </>
-            )}
-          </nav>
-          <section className="relative flex-1">
-            <img
-              className="w-96 absolute bottom-0 right-12"
-              src="/images/bot-2-transformed.png"
-              alt="Photo of two AI bots talking to one another."
-            />
-          </section>
-        </div>
-        <div className="h-96 bg-cgreen-light p-12 flex flex-col gap-2">
-          <h2 className="text-3xl font-bold uppercase">Welcome To OLNX</h2>
-          <p className="font-mono">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
-            placeat perferendis qui consequuntur nesciunt architecto! Odio
-            incidunt porro aut doloremque, dolore id eveniet quidem dignissimos
-            dolorem officiis commodi, recusandae omnis?
+      </div>
+      <div className="mt-20">
+        <div className="w-[65%] m-auto">
+          <h1 className="font-bold text-4xl">Gallery</h1>
+          <p className="pt-5 pb-10">
+            Sample text. Click to select the text box. Click again or double
+            click to start editing the text.
           </p>
-          <div className="flex">
-            <Link
-              href="/login"
-              className="px-6 py-3 bg-cgreen rounded-full border-4 border-cgreen-dark"
-            >
-              Get Started
-            </Link>
-            <span className="flex-1" />
+        </div>
+        <div className="w-[65%] h-[300px] m-auto">
+          <img
+            className="w-[100%] h-[100%]"
+            src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTqt-u-5LXDTN9ZWZ5jdho7KRLVHLiA1v0DYOAZ0i3Kx2n5Y6rM"
+            alt=""
+          />
+        </div>
+        <div className="w-[65%] m-auto pt-4">
+          <img
+            src="https://lh3.googleusercontent.com/8P55VXaz5HXvCeTw7H_W0tawQooMKvJ-NmVosJOfZiOOVTOc6y2kDdtPWDIanbNXLlYmIljOlsOugGPRK1cf5KkBWwduoJJJrexVq6I7"
+            alt=""
+          />
+        </div>
+        <div className="w-[65%] m-auto h-[500px] py-4">
+          <img
+            className="w-[100%] h-[100%]"
+            src="https://lh3.googleusercontent.com/teKdjq06kZ12VT0dp_y3WCbaIhxal9XDASu9tmbw1AVg7mxS3Kxmr4tNIEurv03kOu_evmowcbZBOt39n4htWxj-1AhqIXeweyabIrwJPg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="mt-20 bg-cred-light py-20">
+        <div className="w-[65%] m-auto py-10 px-2 bg-white">
+          <h1 className="text-2xl font-bold">
+            We`ve seen a lot of efficiency in our workflow processes the way
+            subs work and the way designers work with templates.
+          </h1>
+        </div>
+        <p className="text-center text-white text-lg font-bold pt-5">
+          -James Hawkes, General Manager
+        </p>
+        <p className="text-center text-white text-lg font-bold">Operations -</p>
+      </div>
+      <div className="mt-20">
+        <div className="w-[65%] m-auto">
+          <div>
+            <h1 className="font-bold text-3xl">Responsiove Pricing Table</h1>
+            <p className="pt-4 pb-8">
+              Sample text. Click to select the text box. Click again or double
+              click to start editing t
+            </p>
+          </div>
+          <div className="m-auto border">
+            <div className="bg-cred-light py-10 px-5 text-center">
+              <p className="font-bold uppercase text-3xl">basic</p>
+              <p className="font-bold text-3xl pt-5">$ 30</p>
+            </div>
+            <div className="text-center py-5 bg-gray-100">
+              <p className="py-5">1 full user</p>
+              <p className="pb-5">1,000 Email Previews</p>
+              <p className="pb-5">5 contacts per client</p>
+              <p>5 coffee cups</p>
+              <div className="flex justify-center pt-10 pb-5">
+                <button className="border uppercase px-5 text-lg py-1 bg-cred-light hover:bg-cred-dark">
+                  buy now
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 border">
+            <div className="bg-cred-light py-10 px-5 text-center">
+              <p className="font-bold uppercase text-3xl">standard</p>
+              <p className="font-bold text-3xl pt-5">$ 60</p>
+            </div>
+            <div className="text-center py-5">
+              <p className="py-5">10 full user</p>
+              <p className="pb-5">2,000 Email Previews</p>
+              <p className="pb-5">10 contacts per client</p>
+              <p>10 coffee cups</p>
+              <div className="flex justify-center pt-10 pb-5">
+                <button className="border uppercase px-5 text-lg py-1 bg-cred-light hover:bg-cred-dark">
+                  buy now
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="my-5">
+            <img
+              className="w-[100%]"
+              src="https://images01.nicepage.com/a1389d7bc73adea1e1c1fb7e/3bd73aa7b5ae57bab1b9e412/pexels-photo-3331837.jpeg"
+              alt="image"
+            />
           </div>
         </div>
-      </section>
-    </main>
+      </div>
+      <div className="bg-cred-light py-20">
+        <div className="w-[65%] m-auto">
+          <div className="w-[100%]">
+            <img
+              className="w-[100%]"
+              src="https://images01.nicepage.com/a1389d7bc73adea1e1c1fb7e/125ac1f553655a7ea12fdc91/pexels-photo-5803334.jpeg"
+              alt=""
+            />
+          </div>
+          <div className="py-10">
+            <h1 className="font-bold text-3xl">Call us</h1>
+            <p className="pt-3">+91 9234786567</p>
+            <p>+91 7676768978</p>
+          </div>
+          <div>
+            <h1 className="font-bold text-3xl">Location</h1>
+            <p className="pt-3">121 Rock Sreet, 21 Avenue,</p>
+            <p>New York, NY 92103-9000</p>
+          </div>
+          <div className="py-10">
+            <h1 className="font-bold text-3xl">Our top services</h1>
+            <div className="pt-3">
+              <p className="font-bold">Local Trasfers</p>
+              <p className="font-bold">Airport Transfers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 }
+
+export default index;
