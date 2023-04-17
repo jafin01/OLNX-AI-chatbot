2e8974fd69c0598264a5d2c442a78d2459b7453f
Harman Kamboj <playcivil@gmail.com>
2023-04-17 19:32:41 +0530

    feat(deps): add @openreplay/tracker dependency feat(_app.tsx): add OpenReplay tracker to the app and start it on mount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import Tracker from "@openreplay/tracker/cjs";
+import { useEffect } from "react";
+
+const tracker = new Tracker({
+  projectKey: "OOfFclJkE8QmRYP6LCY7",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    tracker.start();
+  }, []);
   return (
     <>
       <Component {...pageProps} />
