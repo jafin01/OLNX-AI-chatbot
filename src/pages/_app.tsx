bab96750ed4b801d88ba109b7a69592160a72631
Harman Kamboj <playcivil@gmail.com>
2023-04-17 19:50:30 +0530

    feat(deps): add @openreplay/tracker-assist dependency feat(tracker): add trackerAssist plugin to Tracker instance in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import Tracker from "@openreplay/tracker";
+import trackerAssist from "@openreplay/tracker-assist";
 import { useEffect } from "react";
 
 const tracker = new Tracker({
   projectKey: "OOfFclJkE8QmRYP6LCY7",
 });
+
+tracker.use(trackerAssist({}));
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
